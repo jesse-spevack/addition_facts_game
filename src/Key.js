c8f4d7676461ad1063437bad6d36d4baa2542f33
PlanetEfficacy <jspevack@gmail.com>
2022-10-16 07:22:14 -0600

    Change focus to active

--- src/Key.js
+++ src/Key.js
@@ -1,3 +1,4 @@
+import { hasSelectionSupport } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
 
 class Key extends Component {
@@ -6,7 +7,7 @@ class Key extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
+  handleClick(e) {
     this.props.handleClick(
       this.props.number
     )
@@ -15,7 +16,7 @@ class Key extends Component {
   render () {
     return (
       <div>
-        <button onClick={this.handleClick} type="button" className="inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <button onClick={this.handleClick} type="button" className="inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm active:bg-indigo-700 focus:outline-none active:ring-2 active:ring-indigo-500 active:ring-offset-2">
           <p className="text-5xl font-mono text-center">
             {this.props.number}
           </p>
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -30,11 +30,11 @@ class Keyboard extends Component {
         <div className="p-7 overflow-hidden rounded-lg shadow">
           <Keys handleClick={this.handleClick} />
           <div className="px-5 py-3 grid grid-cols-3 gap-4">
-            <button onClick={this.delete} type="button" className="px-2 inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <button onClick={this.delete} type="button" className="px-2 inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 active:outline-none active:ring-2 active:ring-indigo-500 active:ring-offset-2">
               <BackspaceIconOutline className="h-14 w-14" area-hidden="true"/>
             </button>    
             <Key handleClick={this.handleClick} number={0}/>
-            <button onClick={this.submit} type="button" className="px-2 inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <button onClick={this.submit} type="button" className="px-2 inline-block items-center h-20 w-20 overflow-hidden rounded-full border border-transparent bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 active:outline-none active:ring-2 active:ring-indigo-500 active:ring-offset-2">
               <CheckIconOutline className="h-14 w-14" area-hidden="true"/>
             </button>
           </div>
