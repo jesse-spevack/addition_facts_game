99a074766e01efe8991f7e7676040b6270df73db
PlanetEfficacy <jspevack@gmail.com>
2022-10-23 21:44:13 -0600

    larger screen adjustments

--- src/AdditionProblem.js
+++ src/AdditionProblem.js
@@ -9,13 +9,13 @@ class AdditionProblem extends Component {
     return (
       <div>
         <div className="grid grid-cols-3 grid-rows-2 gap-1 font-mono text-7xl sm:text-9xl text-right">
-          <div className="col-span-1 row-span-2 sm:border-b-8 sm:ml-14 self-center">
+          <div className="col-span-1 row-span-2 sm:ml-14 self-center">
             <p>+</p>
           </div>
           <div className="col-span-1 row-span-1">
             <p>{this.props.leftOperand}</p>
           </div>
-          <div className="col-span-1 col-start-2 row-span-1 row-start-2 border-b-8 sm:mr-14">
+          <div className="col-span-1 col-start-2 row-span-1 row-start-2 border-b-8">
             <p>{this.props.rightOperand}</p>
           </div>
         </div>
