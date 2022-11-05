0a83cb19c37da9234f91881203f5a01ea972755a
PlanetEfficacy <jspevack@gmail.com>
2022-11-05 08:59:40 -0600

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,90 +1,54 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import Game from "./Game";
 import SignIn from "./SignIn";
 import Stats from "./Stats";
 import VersionFooter from "./VersionFooter";
-// import {db} from "./firebase";
-// import {collection, getDocs, addDoc, updateDoc} from "firebase/firestore";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: false,
-      user: "",
-      showStats: false,
-    };
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showStats, setShowStats] = useState(false);
+  const [username, setUsername] = useState("");
 
-    this.signIn = this.signIn.bind(this);
-    this.signOut = this.signOut.bind(this);
-    this.showStats = this.showStats.bind(this);
-    this.backToGame = this.backToGame.bind(this);
+  const signOut = () => {
+    setIsLoggedIn(false);
+    setUsername("");
+  };
 
-    // const usersCollectionRef = collection(db, "addition_facts")
-    // const getUser = async () => {
-    //   const data = await getDocs(usersCollectionRef)
-    //   console.log(data.docs[0].data())
-    //   console.log(data.docs[0].id)
-    // }
+  const signIn = (username) => {
+    setIsLoggedIn(true);
+    setUsername(username);
+  };
 
-    // getUser()
-  }
+  let screen;
 
-  signIn(username) {
-    this.setState({
-      isLoggedIn: true,
-      user: username,
-    });
-  }
-
-  signOut() {
-    this.setState({
-      isLoggedIn: false,
-      user: "",
-    });
-  }
-
-  showStats() {
-    this.setState({ showStats: true });
-  }
-
-  backToGame() {
-    this.setState({ showStats: false });
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let screen;
-
-    if (isLoggedIn) {
-      if (this.state.showStats) {
-        screen = (
-          <Stats
-            user={this.state.user}
-            rawStats={localStorage.getItem(this.state.user)}
-            back={this.backToGame}
-          />
-        );
-      } else {
-        screen = (
-          <Game
-            user={this.state.user}
-            signOut={this.signOut}
-            showStats={this.showStats}
-          />
-        );
-      }
+  if (isLoggedIn) {
+    if (showStats) {
+      screen = (
+        <Stats
+          user={username}
+          rawStats={localStorage.getItem(username)}
+          back={setShowStats(false)}
+        />
+      );
     } else {
-      screen = <SignIn signIn={this.signIn} />;
+      screen = (
+        <Game
+          user={username}
+          signOut={signOut}
+          showStats={setShowStats(true)}
+        />
+      );
     }
-
-    return (
-      <div className="font-sans flex flex-col min-h-screen text-white bg-slate-900">
-        <div className="flex-grow">{screen}</div>
-        <VersionFooter />
-      </div>
-    );
+  } else {
+    screen = <SignIn signIn={signIn} />;
   }
-}
+
+  return (
+    <div className="font-sans flex flex-col min-h-screen text-white bg-slate-900">
+      <div className="flex-grow">{screen}</div>
+      <VersionFooter />
+    </div>
+  );
+};
 
 export default App;
