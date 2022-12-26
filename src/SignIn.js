cdbbbc899d238b4d2ea32993909e8a653863ecda
PlanetEfficacy <jspevack@gmail.com>
2022-12-25 18:30:49 -0700

    Lowercase username

--- src/SignIn.js
+++ src/SignIn.js
@@ -34,7 +34,7 @@ const SignIn = ({ signIn }) => {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            signIn(username.trim());
+            signIn(username.trim().toLowerCase());
           }}
           autoComplete="off"
         >
